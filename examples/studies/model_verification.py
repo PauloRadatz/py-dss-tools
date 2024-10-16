4b04e6b43d7d72591479c8616e7808859f6147ad
Paulo Radatz <paulo.radatz@gmail.com>
2024-10-16 07:46:05 -0400

    Example created to show Ferdinando

---
+++ examples/studies/model_verification.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Author  : Paulo Radatz
+# @Email   : paulo.radatz@gmail.com
+# @File    : model_verification.py
+# @Software: PyCharm
+
+import os
+import pathlib
+from py_dss_tools import CreateStudy
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+dss_file = pathlib.Path(script_path).joinpath("..", "feeders", "123Bus", "IEEE123Master.dss")
+bus_coords = pathlib.Path(script_path).joinpath("..", "feeders", "123Bus", "buscoords.dat")
+
+# Creat Static study object
+study = CreateStudy.model_verification("My Study", dss_file=dss_file)
+
+# Load bus coordinates for visualization
+study.dss.text(f"buscoords buscoords.dat")
+
+# Add energymeter at the feeder head
+study.model.add_line_in_vsource(add_meter=True)
+
+result_1 = study.results.isolated
+print("No problem added")
+print(result_1)
+
+# Add problem
+study.dss.text("edit Line.L112 bus2=open")
+result_2 = study.results.isolated
+print("\nproblem added")
+print(result_2)
--- src/py_dss_tools/results/ModelVerification/Isolated.py
+++ src/py_dss_tools/results/ModelVerification/Isolated.py
@@ -4,42 +4,76 @@
 # @File    : Isolated.py
 # @Software: PyCharm
 
-from py_dss_interface import DSS
 import pandas as pd
-from dataclasses import dataclass, field
-from typing import Tuple
+from py_dss_interface import DSS
+
 
 class Isolated:
 
     def __init__(self, dss: DSS):
         self._dss = dss
         self._isolated = pd.DataFrame()
+
     @property
     def isolated(self) -> pd.DataFrame:
-        return self.__check_isolated() # Todo - it should return a dataframe with the element names
+        return self.__check_isolated()  # Todo - it should return a dataframe with the element names
+
     def __check_isolated(self):
+        self._dss.text("solve")
+
         branches_isolated = self._dss.topology.all_isolated_branches
         loads_isolated = self._dss.topology.all_isolated_loads
 
+        # Clear NONE to not get into the for loop
+        if "NONE" == branches_isolated[0]:
+            branches_isolated.remove("NONE")
+        if "NONE" == loads_isolated[0]:
+            loads_isolated.remove("NONE")
+
+        data = []
+
         for branch in branches_isolated:
             self._dss.circuit.set_active_element(branch)
-            name = self._dss.cktelement.name
+            name = self._dss.cktelement.name.lower()
             if len(self._dss.cktelement.bus_names) == 2:
                 bus1 = self._dss.cktelement.bus_names[0].split(".")[0]
                 bus2 = self._dss.cktelement.bus_names[1].split(".")[0]
 
-                print(f"{name} bus1: {bus1} bus2: {bus2}")
             else:
                 bus1 = self._dss.cktelement.bus_names[0].split(".")[0]
-                print(f"{name} bus1: {bus1}")
+                bus2 = ""
+
+            data.append([name, bus1, bus2])
 
         for load in loads_isolated:
             self._dss.circuit.set_active_element(load)
-            name = self._dss.cktelement.name
+            name = self._dss.cktelement.name.lower()
             bus1 = self._dss.cktelement.bus_names[0].split(".")[0]
+            bus2 = ""
 
-            print(f"{name} bus1: {bus1}")
+            data.append([name, bus1, bus2])
 
-        self._dss.text("solve")
-        branches_isolated
-        loads_isolated
+        return pd.DataFrame(data, columns=["element name", "bus1", "bus2"])
+
+if __name__ == '__main__':
+    import os
+    import pathlib
+
+    dss = DSS()
+
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    dss_file = pathlib.Path(script_path).joinpath("..", "..","..", "..", "examples", "feeders", "123Bus", "IEEE123Master.dss")
+    bus_coords = pathlib.Path(script_path).joinpath("..", "..","..", "..", "examples", "feeders", "123Bus", "buscoords.dat")
+
+    dss.text(f"compile [{dss_file}]")
+    dss.text(f"buscoords buscoords.dat")
+    dss.text("new energymeter.m element=line.l115")
+
+    result_1 = Isolated(dss).isolated  # To get results.
+
+    # Add problem
+    dss.text("edit Line.L112 bus2=open")
+
+    result_2 = Isolated(dss).isolated
+
+    print("here")
